2b1ed4916a42ac3e72e97b42375c52a7fb4921b9
Gicela <gicela@mexicanwave.comq>
2017-06-17 11:13:29 +0100

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,6 +3,7 @@ var TodoList = require('TodoList');
 
 var AddTodo =require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid= require('node-uuid');
 
 var TodoApp = React.createClass({
     getInitialState: function(){
@@ -11,26 +12,34 @@ var TodoApp = React.createClass({
             searchText:'',
             todos: [
                 {
-                    id:1,
+                    id:uuid(),
                     text: ' Walk the dog'
                 },
                 {
-                    id: 2,
+                    id: uuid(),
                     text: 'clean the patio'
                 },
                 {
-                    id: 3,
+                    id: uuid(),
                     text: 'Wash clothes'
                 },
                 {
-                    id: 4,
+                    id: uuid(),
                     text: 'Check mail'
                 }
             ]
         }
     },
     handleAddTodo: function(text){
-        alert('new todo:' + text);
+        this.setState({
+            todos: [
+                ...this.state.todos,
+                {
+                    id: uuid(),
+                    text:text
+                }
+            ]
+        })
     },
 
     handleSearch: function(showCompleted, searchText){
